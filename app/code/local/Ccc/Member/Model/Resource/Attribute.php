9cde2cdd4b382632af1122a7ec564ff08bd5beda
RimaJogani <rimapatel199808@gmail.com>
2021-05-14 12:18:00 +0530

    update eav error

--- app/code/local/Ccc/Member/Model/Resource/Attribute.php
+++ app/code/local/Ccc/Member/Model/Resource/Attribute.php
@@ -24,7 +24,7 @@ class Ccc_Member_Model_Resource_Attribute extends Mage_Eav_Model_Resource_Entity
 
         if ($object->isScopeGlobal()
             && isset($origData['is_global'])
-            && Ccc_Memberattribute_Model_Resource_Eav_Attribute::SCOPE_GLOBAL != $origData['is_global']
+            && Ccc_Member_Model_Resource_Eav_Attribute::SCOPE_GLOBAL != $origData['is_global']
         ) {
             $attributeStoreIds = array_keys(Mage::app()->getStores());
             if (!empty($attributeStoreIds)) {
@@ -53,7 +53,7 @@ class Ccc_Member_Model_Resource_Attribute extends Mage_Eav_Model_Resource_Entity
 
         if ($result) {
             $attribute = Mage::getSingleton('eav/config')
-                ->getAttribute(Ccc_Memberattribute_Model_Resource_Memberattribute::ENTITY, $result['attribute_id']);
+                ->getAttribute(Ccc_Member_Model_Resource_Eav_Attribute::ENTITY, $result['attribute_id']);
 
             $backendTable = $attribute->getBackend()->getTable();
             if ($backendTable) {
