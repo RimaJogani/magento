cd18f26f245384ccb88d3851e091ffaecbc0b242
RimaJogani <rimapatel199808@gmail.com>
2021-05-20 21:29:43 +0530

    payment method

---
+++ app/code/local/Rima/Paymentmethod/Block/Form/Paymentmethod.php
@@ -0,0 +1,9 @@
+<?php
+class Rima_Paymentmethod_Block_Form_Paymentmethod extends Mage_Payment_Block_Form
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('paymentmethod/form/paymentmethod.phtml');
+    }
+}
---
+++ app/code/local/Rima/Paymentmethod/Model/Cod.php
@@ -0,0 +1,9 @@
+<?php
+
+class Rima_Paymentmethod_Model_Cod extends Mage_Payment_Model_Method_Abstract 
+{
+    protected $_code = 'cod';
+ 
+}
+
+?>
